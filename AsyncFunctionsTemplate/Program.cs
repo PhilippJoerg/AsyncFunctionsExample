f745caf47c580e48f8aa36eb4fb38bb9ac8c69ea
Philipp Jörg <43471899+PhilippJoerg@users.noreply.github.com>
2020-11-06 08:36:52 +0100

    Add project files.

---
+++ AsyncFunctionsTemplate/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AsyncFunctionsTemplate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
