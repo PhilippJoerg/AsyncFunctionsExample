b3386fb098bdcc635d5c8283db03a485b600f29f
Philipp Jörg <43471899+PhilippJoerg@users.noreply.github.com>
2020-11-06 08:49:45 +0100

    #added template

--- AsyncFunctionsTemplate/Program.cs
+++ AsyncFunctionsTemplate/Program.cs
@@ -1,12 +1,74 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace AsyncFunctionsTemplate
 {
     class Program
     {
-        static void Main(string[] args)
+        static Task Main() => SumPageSizesAsync();
+
+        static async Task SumPageSizesAsync()
         {
-            Console.WriteLine("Hello World!");
+            var stopwatch = Stopwatch.StartNew();
+
+            IEnumerable<Task<int>> downloadTasksQuery =
+                from url in s_urlList
+                select ProcessUrlAsync(url, s_client);
+
+            List<Task<int>> downloadTasks = downloadTasksQuery.ToList();
+
+            int total = 0;
+            while (downloadTasks.Any())
+            {
+                Task<int> finishedTask = await Task.WhenAny(downloadTasks);
+                downloadTasks.Remove(finishedTask);
+                total += await finishedTask;
+            }
+
+            stopwatch.Stop();
+
+            Console.WriteLine($"\nTotal bytes returned:  {total:#,#}");
+            Console.WriteLine($"Elapsed time:          {stopwatch.Elapsed}\n");
         }
+
+        static async Task<int> ProcessUrlAsync(string url, HttpClient client)
+        {
+            byte[] content = await client.GetByteArrayAsync(url);
+            Console.WriteLine($"{url,-60} {content.Length,10:#,#}");
+
+            return content.Length;
+        }
+
+        static readonly HttpClient s_client = new HttpClient
+        {
+            MaxResponseContentBufferSize = 1_000_000
+        };
+
+        static readonly IEnumerable<string> s_urlList = new string[]
+        {
+            "https://docs.microsoft.com",
+            "https://docs.microsoft.com/aspnet/core",
+            "https://docs.microsoft.com/azure",
+            "https://docs.microsoft.com/azure/devops",
+            "https://docs.microsoft.com/dotnet",
+            "https://docs.microsoft.com/dynamics365",
+            "https://docs.microsoft.com/education",
+            "https://docs.microsoft.com/enterprise-mobility-security",
+            "https://docs.microsoft.com/gaming",
+            "https://docs.microsoft.com/graph",
+            "https://docs.microsoft.com/microsoft-365",
+            "https://docs.microsoft.com/office",
+            "https://docs.microsoft.com/powershell",
+            "https://docs.microsoft.com/sql",
+            "https://docs.microsoft.com/surface",
+            "https://docs.microsoft.com/system-center",
+            "https://docs.microsoft.com/visualstudio",
+            "https://docs.microsoft.com/windows",
+            "https://docs.microsoft.com/xamarin"
+        };
     }
 }
